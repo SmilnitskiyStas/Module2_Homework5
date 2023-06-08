a77dccf92a3be6a65493dad5e72d1aed0562424f
SmilnitskiyStas <stassmilnitskiy@gmail.com>
2023-06-08 23:40:02 +0300

    Implemented all functionality from the task. The project is finished.

---
+++ Logger_#2/Logger_#2/DataWriter/FileService.cs
@@ -0,0 +1,119 @@
+﻿using System.Text;
+using Logger__2.Models;
+using Newtonsoft.Json;
+
+namespace Logger__2.DataWriter
+{
+    internal class FileService
+    {
+        public string Message { get; set; }
+
+        public string FileName { get; set; }
+
+        private PathDirectory PathDirectory { get; set; }
+
+        public FileService(string fileName)
+        {
+            DeserializeJSONConfigFile();
+
+            FileName = fileName;
+
+            StringLogContent();
+
+            CreateDirectory();
+
+            CheckingFiles();
+        }
+
+        /// <summary>
+        /// Запис в файл списку логів.
+        /// </summary>
+        public void WriteLogInTheFile()
+        {
+            using (StreamWriter fileStream = new StreamWriter($"{PathDirectory.PathDirectoryForSaveFiles}/{FileName}", false))
+            {
+                fileStream.Write(Message);
+            }
+        }
+
+        /// <summary>
+        /// Преобразування із масиву в StringBuilder.
+        /// </summary>
+        private void StringLogContent()
+        {
+            string[] logMessage = Logger.Instance().GetDataLog();
+
+            StringBuilder stringBuilder = new StringBuilder();
+
+            for (int i = 0; i < logMessage.Length; i++)
+            {
+                stringBuilder.AppendLine(logMessage[i]);
+            }
+
+            Message = stringBuilder.ToString();
+        }
+
+        /// <summary>
+        /// Створення папки для файлів із логами.
+        /// </summary>
+        private void CreateDirectory()
+        {
+            if (!Directory.Exists(PathDirectory.PathDirectoryForSaveFiles))
+            {
+                Directory.CreateDirectory(PathDirectory.PathDirectoryForSaveFiles);
+            }
+        }
+
+        /// <summary>
+        /// Перевірка файлів із логами, та відалення найстарішого файлу.
+        /// </summary>
+        private void CheckingFiles()
+        {
+            bool isWorking = true;
+
+            while (true)
+            {
+                FileInfo fileInfo;
+
+                string[] files = Directory.GetFiles(PathDirectory.PathDirectoryForSaveFiles, PathDirectory.SearchFileWithExtension);
+
+                DateTime temp = new FileInfo(files[0]).LastWriteTime;
+
+                int indexForDelete = 0;
+
+                if (files.Length <= 2)
+                {
+                    isWorking = false;
+                    break;
+                }
+                else
+                {
+                    for (int i = 1; i < files.Length; i++)
+                    {
+                        fileInfo = new FileInfo(files[i]);
+
+                        DateTime dateTime = fileInfo.LastWriteTime;
+
+                        if (dateTime < temp)
+                        {
+                            indexForDelete = i;
+                            temp = dateTime;
+                        }
+                    }
+
+                    fileInfo = new FileInfo(files[indexForDelete]);
+                    fileInfo.Delete();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Отримуємо інформацію із Config.json файлу (шлях директорії для запису файлів, розширення файлів, які потрібно шукати).
+        /// </summary>
+        private void DeserializeJSONConfigFile()
+        {
+            var config = File.ReadAllText(@"C:\Users\Stas2\source\repos\GitProject\Module2_Homework5\Logger_#2\Logger_#2\Appconfig.json");
+            PathDirectory = JsonConvert.DeserializeObject<PathDirectory>(config);
+        }
+    }
+}
---
+++ Logger_#2/Logger_#2/Models/Starter.cs
@@ -0,0 +1,61 @@
+﻿using System.Text;
+using Logger__2.DataWriter;
+using Logger__2.Enums;
+using Logger__2.Exceptions;
+
+namespace Logger__2.Models
+{
+    internal class Starter
+    {
+        private readonly string _fileName;
+
+        public Starter()
+        {
+            _fileName = $"{DateTime.Now.ToString("hh.mm.ss dd.MM.yyyy")}.txt";
+        }
+
+        public void Run()
+        {
+            Random random = new Random();
+
+            Actions actions = new Actions();
+
+            Result result = null;
+
+            for (int i = 0; i < 100; i++)
+            {
+                try
+                {
+                    switch (random.Next(0, 3))
+                    {
+                        case 0:
+                            result = actions.InfoMethod();
+                            break;
+                        case 1:
+                            actions.WarningMethod();
+                            break;
+                        case 2:
+                            actions.ErrorMethod();
+                            break;
+                    }
+                }
+                catch (BusinessException businessException)
+                {
+                    Console.WriteLine(businessException.Message);
+
+                    Logger.Instance().WriteLog(EnumLogStatus.Warning, $"Action got this custom Exception: {businessException.Message}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message, ex.Source);
+
+                    Logger.Instance().WriteLog(EnumLogStatus.Error, $"Action failed by reason: {ex.Source}");
+                }
+            }
+
+            FileService fileService = new FileService(_fileName);
+
+            fileService.WriteLogInTheFile();
+        }
+    }
+}
